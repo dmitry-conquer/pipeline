8106a6cfe23fd71830779bf6a0b48cfa7a62b0e2
Dmitry Conquer <dmitry.conquer@gmail.com>
2024-01-20 11:56:29 +0200

    Створена шапка сайта

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,19 @@
-// import { consoleInfo, isWebp() } from './config/functions.js';
-// import { hasErrors } from './modules/forms.js';
-// import { initSpoiler, initDdropdown } from './modules/spoilers.js';
-// import { initTabs } from './modules/tabs.js';
-// import { initSliders } from './modules/sliders.js';
-// import { initHeader } from './modules/header.js';
-// import { initModals, openModalIf } from './modules/modals.js';
-// import { useDynamicAdapt } from './modules/dynamicAdapt.js';
-// import gsap from "gsap";
-// import { ScrollTrigger } from 'gsap/ScrollTrigger.js';
-// import AOS from 'aos';
-// import Rellax from "rellax";
-// import fslightbox from 'fslightbox';
-// import Ellipsis from 'ellipsis.js';
-// import Choices from 'choices.js';
-// import { CountUp } from '../../node_modules/countup.js/dist/countUp.js';
-// import noUiSlider from 'nouislider';
-// import MicroModal from 'micromodal';
+import { useDynamicAdapt } from "./modules/dynamicAdapt.js";
 
 function app() {
-}
-document.addEventListener('DOMContentLoaded', app);
+  useDynamicAdapt("max");
 
+
+  /**
+   * Burger menu
+   */
+  const burgToggleBtn = document.getElementById("burg-toggle");
+  const headerBody = document.getElementById("header-body");
+  if (burgToggleBtn) {
+    burgToggleBtn.addEventListener("click", () => {
+      burgToggleBtn.classList.toggle("active");
+      headerBody.classList.toggle("active");
+    });
+  }
+}
+document.addEventListener("DOMContentLoaded", app);
