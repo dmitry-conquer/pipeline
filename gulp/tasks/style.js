7e32c731946eba346d38d55035f86aff186abf1a
Dmitry Conquer <dmitry.conquer@gmail.com>
2024-01-21 20:29:50 +0200

    gulp css

--- gulp/tasks/style.js
+++ gulp/tasks/style.js
@@ -1,34 +1,35 @@
-import dartSass from 'sass';
-import gulpSass from 'gulp-sass';
-import postcss from 'gulp-postcss';
-import autoprefixer from 'autoprefixer';
-import cssnano from 'cssnano';
-import combineMediaQueries from 'postcss-combine-media-query';
+import dartSass from "sass";
+import gulpSass from "gulp-sass";
+import postcss from "gulp-postcss";
+import autoprefixer from "autoprefixer";
+import cssnano from "cssnano";
+import combineMediaQueries from "postcss-combine-media-query";
+import gcmq from "gulp-group-css-media-queries";
 
 const sass = gulpSass(dartSass);
 
 export const style = () => {
-   const plugins = [
-      autoprefixer({ grid: true, overrideBrowserslist: 'last 2 versions' }),
-      cssnano(),
-      combineMediaQueries(),
-   ];
+  const plugins = [
+    autoprefixer({ grid: true, overrideBrowserslist: "last 2 versions" }),
+    cssnano(),
+    // combineMediaQueries(),
+  ];
 
-   return app.gulp
-      .src(app.path.src.scss, { sourcemaps: app.isDev })
-      .pipe(
-         sass({
-            outputStyle: 'expanded',
-         }),
-      )
-
-      .pipe(app.plugins.if(app.isBuild, postcss(plugins)))
-      .pipe(
-         app.plugins.rename({
-            extname: '.min.css',
-         }),
-      )
-      .pipe(app.plugins.if(app.isBuild, app.plugins.size({ title: 'CSS', showFiles: true })))
-      .pipe(app.gulp.dest(app.path.build.css, { sourcemaps: app.isDev ? '.' : false }))
-      .pipe(app.plugins.browserSync.stream());
+  return app.gulp
+    .src(app.path.src.scss, { sourcemaps: app.isDev })
+    .pipe(
+      sass({
+        outputStyle: "expanded",
+      }),
+    )
+    .pipe(gcmq())
+    .pipe(app.plugins.if(app.isBuild, postcss(plugins)))
+    .pipe(
+      app.plugins.rename({
+        extname: ".min.css",
+      }),
+    )
+    .pipe(app.plugins.if(app.isBuild, app.plugins.size({ title: "CSS", showFiles: true })))
+    .pipe(app.gulp.dest(app.path.build.css, { sourcemaps: app.isDev ? "." : false }))
+    .pipe(app.plugins.browserSync.stream());
 };
